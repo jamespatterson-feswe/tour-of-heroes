227263bafe50e59f1a2b5e720af90e9325bfb474
James <jamespatterson.feswe@yahoo.com>
2023-03-29 11:20:57 -0400

    chore: update scss files to use variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <app-main></app-main> `,
+  template: `<app-main></app-main>`,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
